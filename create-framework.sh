a23832b25881f0a838a07d7290091144349ffafa
Jose Blaya <ueshiba@gmail.com>
2020-06-18 18:40:18 +0200

    update

--- create-framework.sh
+++ create-framework.sh
@@ -2,8 +2,7 @@
 
 set -euo pipefail
 
-REPO_ROOT="$PODS_TARGET_SRCROOT"
-"$REPO_ROOT/regions/../gradlew" -p "$REPO_ROOT" :regions:syncFramework \
+"$regions/../gradlew" :regions:syncFramework \
     -Pkotlin.native.cocoapods.target=$KOTLIN_TARGET \
     -Pkotlin.native.cocoapods.configuration=$CONFIGURATION \
     -Pkotlin.native.cocoapods.cflags="$OTHER_CFLAGS" \
