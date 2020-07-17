34d8d6d09ac668b3a8e58a52ccedaae27813195a
Juan Docal <juandocal@Juans-MacBook-Pro.local>
2020-07-17 10:13:38 +1000

    fix and bump version

--- regions/src/commonMain/kotlin/com/privateinternetaccess/common/regions/internals/RegionsCommon.kt
+++ regions/src/commonMain/kotlin/com/privateinternetaccess/common/regions/internals/RegionsCommon.kt
@@ -92,7 +92,11 @@ public class RegionsCommon(
     private fun fetchAsync(
             callback: (response: RegionsResponse?, error: Error?) -> Unit
     ) = launch {
-        handleFetchResponse(client.get(ENDPOINT), callback)
+        try {
+            handleFetchResponse(client.get(ENDPOINT), callback)
+        } catch (exception: Exception) {
+            callback(knownRegionsResponse, Error("Error fetching next generation servers: ${exception.message}"))
+        }
     }
 
     public suspend fun handleFetchResponse(
